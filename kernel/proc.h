895f95ede290b8bde10550805a0143348469a92f
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2026-04-14 08:56:46 +0900

    Adding fields for EEVDF

--- kernel/proc.h
+++ kernel/proc.h
@@ -103,7 +103,13 @@ struct proc
   int killed;           // If non-zero, have been killed
   int xstate;           // Exit status to be returned to parent's wait
   int pid;              // Process ID
-  int nice;
+  int nice;             // Nice value
+  int weight;           // Weight value - should be hardcoded
+  uint64 runtime;
+  uint64 vruntime;
+  uint64 vdeadline;
+  int remain_slice;
+  int eligible;
 
   // wait_lock must be held when using this:
   struct proc *parent; // Parent process
