a81b1ca504dfc83f52d0a252c074452b6a305e70
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2026-04-14 01:26:14 +0900

    Added test

--- user/mytest.c
+++ user/mytest.c
@@ -10,16 +10,29 @@ int main(void)
     printf("my pid = %d\n", getpid());
     printf("default nice = %d\n", getnice(getpid()));
 
-    printf("setnice -> %d\n", setnice(getpid(), 10));
+    printf("setnice(getpid(), 10) -> %d\n", setnice(getpid(), 10));
     printf("new nice = %d\n", getnice(getpid()));
+
+    printf("setnice(getpid(), -1) -> %d\n", setnice(getpid(), -1));
+    printf("setnice(getpid(), 40) -> %d\n", setnice(getpid(), 40));
+    printf("setnice(9999, 10) -> %d\n", setnice(9999, 10));
+
+    printf("getnice(9999) -> %d\n", getnice(9999));
 
     printf("all processes:\n");
     ps(0);
+
+    printf("ps(9999): should print nothing below\n");
+    ps(9999);
+    printf("done ps(9999)\n");
+
+    printf("waitpid(9999) -> %d\n", waitpid(9999));
 
     pid = fork();
     if (pid == 0)
     {
         printf("child pid = %d\n", getpid());
+        printf("child nice = %d\n", getnice(getpid()));
         exit(0);
     }
     else
